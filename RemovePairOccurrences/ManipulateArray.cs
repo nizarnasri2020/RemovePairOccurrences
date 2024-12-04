822bcf3c6a0c1f6e1ae602aa8b10a7029cea8fbc
Nasri <nasrinizar20@gmail.com>
2024-12-04 17:55:09 +0100

    Implementer la methode RemoveOccurrences

--- RemovePairOccurrences/ManipulateArray.cs
+++ RemovePairOccurrences/ManipulateArray.cs
@@ -1,16 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RemovePairOccurrences
+﻿namespace RemovePairOccurrences
 {
     public class ManipulateArray
     {
+        public static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enterez une liste des entiers avec espaces ");
+
+        
+            string inputString = Console.ReadLine();
+            string[] inputArray = inputString.Split(' ');
+
+            
+            int[] input = Array.ConvertAll(inputArray, int.Parse);
+
+           
+            int[] result = RemoveOccurrences(input);
+
+           
+            Console.WriteLine("List apres manipulation:");
+           
+        }
         public static int[] RemoveOccurrences(int[] input)
         {
-            return input.ToArray();
+            try
+            {
+                // Count occurrences of elements 
+                var occCount = new Dictionary<int, int>();
+
+                foreach (var num in input)
+                {
+                    if (occCount.ContainsKey(num))
+                    {
+                        occCount[num]++;
+                    }
+                    else
+                    {
+                        occCount[num] = 1;
+                    }
+                }
+
+
+                var result = new List<int>();
+                foreach (var num in input)
+                {
+                    if (occCount[num] % 2 != 0)
+                    {
+                        result.Add(num);
+                    }
+                }
+
+                return result.ToArray();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+           
         }
-        }
+
+    }
 }
